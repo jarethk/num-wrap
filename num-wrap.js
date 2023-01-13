55b4f62779fa71c9d77c761a47c843a422d8ea28
jarethk <43247778+jarethk@users.noreply.github.com>
2023-01-12 21:35:21 -0500

    documenting function

--- num-wrap.js
+++ num-wrap.js
@@ -1,3 +1,19 @@
+/**
+ * Wrap-around a number within the min/max bounds provided, such that if the number is above max it restarts at min, 
+ * and if the number is below min it restarts at max
+ *
+ * @static
+ * @memberOf _
+ * @since 1.0.0
+ * @param {number} val The number to wrap.
+ * @param {number} min The lower bound of the range
+ * @param {number} max The upper bound of the range
+ * @returns {number} Returns the number as it wraps in the range.
+ * @example
+ *
+ * wrap(8, 1, 7);
+ * // => 1
+ */
 export function wrap(val, min, max) {
     let dist = (max - min) + 1;
     if (val < min) {
